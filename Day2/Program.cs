5d6d7d4c3dd91f0052a1ad628f8c5b3314a2a0ed
Machillka <84647953+Machillka@users.noreply.github.com>
2023-10-29 08:47:39 +0800

    update

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+
+namespace ConsoleIn
+{
+    class MainFunction
+    {
+        static void Main(string[] args)
+        {
+            // use the console to make a output
+            Console.WriteLine("Hello world!");
+            // string ?input = Console.ReadLine();
+            var input = Console.ReadLine();
+            Console.WriteLine(input);
+        }
+    }
+}
