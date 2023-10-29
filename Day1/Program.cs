4fb51fc3993e1f52812cbc4fab17dbadd7fb1e24
Machillka <84647953+Machillka@users.noreply.github.com>
2023-10-29 08:21:14 +0800

    Init and Push day1

---
+++ Day1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleIn
+{
+    class MainFunction
+    {
+        static void Main(string[] args)
+        {
+            // use the console to make a output
+            Console.WriteLine("Hello world!");
+
+            string ?input = Console.ReadLine();
+
+            Console.WriteLine(input);
+        }
+    }
+}
