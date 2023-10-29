f45461221a68ae010ceb735dc3d2477a2e9425d6
Machillka <84647953+Machillka@users.noreply.github.com>
2023-10-29 08:28:39 +0800

    Upload the day 2 code

---
+++ Day2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day2
+{
+    class MainFunction
+    {
+        static void Main(string[] arg)
+        {
+            Console.WriteLine("Day2");
+            Console.ReadKey();
+        }
+    }
+}
